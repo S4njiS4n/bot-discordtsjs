f5ee6686a8e072ccc44daebd3161423e59406a70
unknown <jean.larroque.gw2@gmail.com>
2020-03-04 15:19:48 +0100

    MàJ main.js/Ajout arguments commande

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const { Client } = require("discord.js");
-const { TOKEN, PREFIX } = require("./config");
-const client = new Client({ disableEveryone: true });
+const { Client } = require("discord.js"); // ajout de la librairie disocrd.js
+const { TOKEN, PREFIX } = require("./config"); // configuration via le fichier config.js
+const client = new Client({ disableEveryone: true }); // instanciation du client discord.js et exlusion des messages @EveryOne depuis le bot
 
-client.on("ready", () => {
+client.on("ready", () => { // traitement de l'évent BotOnline
   console.log("Je suis prêt !");
 });
 
-client.on("message", msg => {
-  const args = msg.content.split(/ +/g);
-  const cmd = args.shift().toLowerCase();
+client.on("message", msg => { // traitement de l'évent NouveauMessage
+  if (msg.author.bot) return; // exclusion du message du bot
+  const args = msg.content.split(/ +/g); // séparation en argument de tous les groupes de mots issus d'une même commande
+  const cmd = args.shift().toLowerCase(); // suppression du préfixe de la commande
   if (cmd === `${PREFIX}ping`) msg.channel.send("Pong!");
   if (cmd === `${PREFIX}pong`) msg.channel.send("Ping!");
 });
 
-client.login(TOKEN);
+client.login(TOKEN); // login du client instancié du bot au serveur discord
